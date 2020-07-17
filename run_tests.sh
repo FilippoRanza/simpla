0d0521443801a0541e5da824877f1f3d081009af
Filippo Ranza <filipporanza@gmail.com>
2020-07-18 01:00:35 +0200

    now simpla_parser is tested before simpla

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# run simpla_parser test before simpla (the full compiler) tests
+# this can be achieved also by hand, but in this way is easier for Travis
+
+cd simpla_parser
+cargo build --verbose --all
+cargo test --verbose --all --  --nocapture
+
+cd ..
+cargo build --verbose --all
+cargo test --verbose --all --  --nocapture
