2db03eab9cd197f15672b5e6735b370ecb7275cf
Filippo Ranza <filipporanza@gmail.com>
2020-07-18 01:04:00 +0200

    now run_tests.sh fails at the first failing test

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,8 @@
 # run simpla_parser test before simpla (the full compiler) tests
 # this can be achieved also by hand, but in this way is easier for Travis
 
+set -e 
+
 cd simpla_parser
 cargo build --verbose --all
 cargo test --verbose --all --  --nocapture
