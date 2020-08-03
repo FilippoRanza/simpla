49573473fb014d1a797c4bf1190ef4e2ad0412c1
Filippo Ranza <filipporanza@gmail.com>
2020-08-03 12:15:25 +0200

    now can load source file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use structopt::StructOpt;
 mod semantic_analysis;
-
 use simpla_parser;
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::Read;
+
+#[derive(StructOpt)]
+struct Arguments {
+    source_file: PathBuf,
+}
+
+fn load_file(file: &Path) -> std::io::Result<String> {
+
+    let mut file = File::open(file)?;
+    let mut output = String::new();
+    file.read_to_string(&mut output)?;
+
+    Ok(output)
+
+}
 
 fn compile(code: &str) -> Result<(), String> {
     let parser = simpla_parser::ProgramParser::new();
@@ -9,7 +27,9 @@ fn compile(code: &str) -> Result<(), String> {
 }
 
 fn main() {
-    match compile("") {
+    let args = Arguments::from_args();
+    let prog = load_file(&args.source_file).unwrap();
+    match compile(&prog) {
         Ok(()) => {}
         Err(err_msg) => eprintln!("{}", err_msg),
     }
